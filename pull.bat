280cb727214436390ccf8161add9c23f53cd7e11
in-sol-ence <sol.oberoi.m@gmail.com>
2024-07-23 19:07:51 -0500

    modified pull.bat

--- pull.bat
+++ pull.bat
@@ -1,26 +1,25 @@
 echo off
 setlocal enabledelayedexpansion
 
+git stash push -m "Stashed untracked files" --include-untracked
 git fetch
-for /f "tokens=0 delims= " %%I in (git checkout -b temp origin/master) do (
-    if (%%I == "fatal"):
-        echo There are no commits as of now
-        goto :end
-    else :
-        goto :SECOND
-)
 
-:SECOND
-echo Recieaved commits
-for /f "tokens=0 delims= " %%I in (git checkout master) do (
-    if (%%I == "error"):
-        echo Your local's main branch is 'main' not master. Go to Sol to fix the bat script
-        goto :end
-    git reset --hard temp
-    git branch -d temp
-    echo Succesfully replaced working directory
+git checkout -b temp origin/main 2>nul
+if errorlevel == 1 (
+    echo There are no commits as of now
     goto :end
-)
+) else {
+    echo Recieaved commits
+}
+git checkout master 2>nul
+if errorlevel == 1 :
+    echo Your main branch isnt master. Talk to Sol to fix this.
+    goto :end
+
+git reset --hard temp
+git branch -d temp
+echo Succesfully replaced working directory
+goto :end
 
 :end
 echo Ending your pull request
